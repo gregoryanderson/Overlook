c4093f4f6bfcb502b4503bde1a9bf8a7f66cf744
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-07-25 18:59:55 -0600

    Finish Base

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 import $ from 'jquery';
 import './css/base.scss';
-import './images/turing-logo.png'
+// import domUpdates from './domUpdates';
+
+import Main from '../src/main';
+// import UserRepo from '../src/UserRepo'
+// import Customer from './Customer';
+// import Room from '../src/Room';
+// import RoomServiceRepo from './RoomServiceRepo';
+// import RoomService from '../src/RoomService';
+// import BookingRepo from './BookingsRepo';
+// import Booking from './Booking'
+// import domUpdates from './domUpdates';
+// import data from './data'
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
+// import Admin from './Admin';
+
+let customers = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users').then(function(response) {
+  return response.json()
+});
+
+let services = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices').then(function(response) {
+  return response.json()
+});
+
+let bookings = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings').then(function(response) {
+  return response.json()
+});
+
+let rooms = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms').then(function(response) {
+  return response.json()
+});
+
+let allData = {'customers': {}, 'roomServices': {}, 'bookings': {}, 'rooms': {}}
+
+Promise.all([customers, services, bookings, rooms])
+  .then(function(values) {
+    allData['customers'] = values[0];
+    allData['services'] = values[1];
+    allData['bookings'] = values[2];
+    allData['rooms'] = values[3];
+    return allData;
+  })
+  .then(console.log(allData))
+  .catch(error => console.log(`Error in promises ${error}`));
 
 $(document).ready(function () {
     let userRepo;
@@ -14,22 +55,28 @@ $(document).ready(function () {
     let roomService;
     let customerBooking;
 
-
+    
     const todaysDate = () => {
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth() + 1;
-        let yyyy = today.getFullYear();
-    
-        if (dd < 10) {
-          dd = '0' + dd
-        }
-    
-        if (mm < 10) {
-          mm = '0' + mm
-        }
-    
-        let thisDay = dd + '/' + mm + '/' + yyyy;
-        return thisDay;
+      let today = new Date();
+      let dd = today.getDate();
+      let mm = today.getMonth() + 1;
+      let yyyy = today.getFullYear();
+      
+      if (dd < 10) {
+        dd = '0' + dd
       }
-    });
+      
+      if (mm < 10) {
+        mm = '0' + mm
+      }
+      
+      let thisDay = dd + '/' + mm + '/' + yyyy;
+      return thisDay;
+    };
+
+
+    setTimeout( () => {
+      main = new Main(allData.customers, allData.bookings, allData.services, allData.rooms, todaysDate())
+      console.log(main)
+    }, 3000);
+  });
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import domUpdates from "./domUpdates";
 
 class Main {
-    constructor (){
-        
+    constructor (customers, bookings, services, rooms, date){
+        this.customers = customers;
+        this.bookings = bookings;
+        this.services = services;
+        this.rooms = rooms;
+        this.date = date;
+    }
+
+    roomsAvailable(todaysDate){
+        return this.bookings.filter(booking => {
+            if(booking.date === todaysDate){
+                return booking;
+            }
+        })
+    }
+
+    updateToDom(todaysDate){
+        domUpdates.displayRoomsAvailable(this.roomsAvailable);
+        domUpdates.displayDate(todaysDate)
+
+
     }
 
 }
